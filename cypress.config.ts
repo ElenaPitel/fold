c7de4ca7337d8d1cd13fe1af7392e56954a2a80c
ElenaPitel <9834893@gmail.com>
2023-04-26 20:40:51 -0500

    add reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+    video: false,
+    retries: 1,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
     baseUrl: "http://localhost:3000/",
   },
-});
+})
